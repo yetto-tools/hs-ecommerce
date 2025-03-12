8a3a7e948a9e147963fcfce39e9b166b5b70d90a
Erick Orlando Rashón González <erashon@disresa.com>
2025-03-11 20:43:21 -0600

    correccion y fix de modal swiper

--- src/components/product/ProductModal2.jsx
+++ src/components/product/ProductModal2.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,15 +7,16 @@ import { addToCart } from "../../store/slices/cart-slice";
 import { Modal } from "react-bootstrap";
 import { CurrencyFormatter } from "../../helpers/currencyFormatter";
 import { VariantSelector } from "./VariantSelector";
+
 import Swiper, { SwiperSlide } from "../../components/swiper";
 
 import { ROOT_IMAGE } from "../../config";
 import { EffectFade, Navigation, Thumbs } from "swiper";
 
-
 function ProductModal2({ show, onHide, currency }) {
   const dispatch = useDispatch();
   const Navigate = useNavigate();
+
   const { articleDetail } = useSelector((state) => state.articleDetail);
   const [selectedVariant, setSelectedVariant] = useState(null);
   const [productStock, setProductStock] = useState(0);
@@ -51,28 +52,30 @@ function ProductModal2({ show, onHide, currency }) {
   const thumbnailSwiperParams = {
     onSwiper: setThumbsSwiper,
     spaceBetween: 10,
-    slidesPerView: 2,
+    slidesPerView: 4,
     touchRatio: 0.2,
     freeMode: true,
     loop: true,
-    autoplay: true,
     slideToClickedSlide: true,
-  
-    modules: [Thumbs],
+    navigation: true,
+  };
+  const onCloseModal = () => {
+    setThumbsSwiper(null);
+    onHide();
   };
   return (
     <Modal
       show={show}
-      onHide={onHide}
+      onHide={onCloseModal}
       className="product-quickview-modal-wrapper"
     >
-      <Modal.Header closeButton />
+      <Modal.Header closeButton></Modal.Header>
       <Modal.Body>
         <div className="modal-body">
           <div className="row">
             <div className="col-md-5 col-sm-12 col-xs-12">
               <div className="product-large-image-wrapper px-4">
-                <Swiper options={gallerySwiperParams} >
+                <Swiper options={gallerySwiperParams}>
                   {articleDetail.images &&
                     articleDetail.images.map((image, index) => (
                       <SwiperSlide key={index}>
@@ -86,18 +89,7 @@ function ProductModal2({ show, onHide, currency }) {
                 </Swiper>
               </div>
               <div className="product-small-image-wrapper mt-40" id="thumbnail">
-              <Swiper
-                onSwiper={setThumbsSwiper}
-                spaceBetween={10}
-                slidesPerView={4}
-                touchRatio={0.2}
-                freeMode={true}
-                loop={true}
-                autoplay={{ delay: 2500 }}
-                slideToClickedSlide={true}
-                navigation={true}
-                modules={[Thumbs, Navigation]} // ✅ Asegura que se pasan los módulos correctamente
-              >
+                <Swiper options={thumbnailSwiperParams}>
                   {articleDetail.image &&
                     articleDetail.images.map((image, i) => {
                       return (
@@ -118,12 +110,27 @@ function ProductModal2({ show, onHide, currency }) {
             <div className="col-md-7 col-sm-12 col-xs-12">
               <div className="product-details-content quickview-content">
                 <h2>{articleDetail.name}</h2>
-                <div className="product-details-price">
-                  {CurrencyFormatter(
-                    articleDetail.discountedPrice || articleDetail.price,
-                    i18n,
-                    currency
-                  )}
+                <div className="product-details-price gap-4">
+                  <span className="fs-4 fw-bold">
+                    {CurrencyFormatter(
+                      articleDetail.discountedPrice || articleDetail.price,
+                      i18n,
+                      currency
+                    )}
+                  </span>
+                  <span>
+                    {articleDetail && articleDetail?.discount > 0 && (
+                      <del>
+                        <small className="text-muted">
+                          {CurrencyFormatter(
+                            articleDetail.price,
+                            i18n,
+                            currency
+                          )}
+                        </small>
+                      </del>
+                    )}
+                  </span>
                 </div>
                 <VariantSelector
                   articleDetail={articleDetail}
@@ -141,35 +148,45 @@ function ProductModal2({ show, onHide, currency }) {
                     gridRowGap: "1.5rem",
                   }}
                 >
-                  <div className="cart-plus-minus">
-                    <button
-                      className="dec qtybutton text-black"
-                      disabled={quantityCount <= 1}
-                      onClick={() =>
-                        setQuantityCount((count) => Math.max(1, count - 1))
-                      }
-                    >
-                      -
-                    </button>
-                    <input
-                      className="cart-plus-minus-box text-black text-center"
-                      type="number"
-                      value={quantityCount}
-                      readOnly
-                    />
+                  {articleDetail.stock > 0 ? (
+                    <div className="cart-plus-minus">
+                      <>
+                        <button
+                          className="dec qtybutton text-black"
+                          disabled={quantityCount <= 1}
+                          onClick={() =>
+                            setQuantityCount((count) => Math.max(1, count - 1))
+                          }
+                        >
+                          -
+                        </button>
+                        <input
+                          className="cart-plus-minus-box text-black text-center"
+                          type="number"
+                          value={quantityCount}
+                          readOnly
+                        />
 
-                    <button
-                      className="inc qtybutton text-black"
-                      disabled={quantityCount >= productStock}
-                      onClick={() =>
-                        setQuantityCount((count) =>
-                          Math.min(productStock, count + 1)
-                        )
-                      }
-                    >
-                      +
-                    </button>
-                  </div>
+                        <button
+                          className="inc qtybutton text-black"
+                          disabled={quantityCount >= productStock}
+                          onClick={() =>
+                            setQuantityCount((count) =>
+                              Math.min(productStock, count + 1)
+                            )
+                          }
+                        >
+                          +
+                        </button>
+                      </>
+                    </div>
+                  ) : (
+                    <span className="d-flex justify-content-start align-items-center">
+                      <span className=" border fs-4 fw-bold p-2">
+                        Sin Existencias
+                      </span>
+                    </span>
+                  )}
                   <div className="pro-details-cart ">
                     <div className="d-flex align-items-center justify-content-start gap-5 mt-3">
                       <button
