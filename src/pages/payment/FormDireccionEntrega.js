2b16281143658239dade56d632ab9a0ccceffaf2
Erick Orlando Rashón González <erashon@disresa.com>
2025-03-30 12:31:03 -0600

    Fix Formulario direccion

--- src/pages/payment/FormDireccionEntrega.js
+++ src/pages/payment/FormDireccionEntrega.js
@@ -1,39 +1,54 @@
-
-
 import { MapPinPlus } from "lucide-react";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import FormNuevaDireccion from "../../components/address/FormNuevaDireccion";
-
-
+import { useEffect } from "react";
 
 export const FormDireccionEntrega = ({
   country,
+  setFormValues,
   formValues,
   handleChange,
   errorsValidate,
+  setShowAddressNew,
+  showAddressNew,
 }) => {
   const { t } = useTranslation();
-  const {address } = useSelector((state) => state.usuario);
-  const [showAddressNew, setShowAddressNew] = useState(false);
-
+  const { address } = useSelector((state) => state.usuario);
+  useEffect(() => {
+    if (address.length > 0) {
+      setFormValues((prevValues) => ({
+        ...prevValues,
+        idDireccion: address[address.length - 1].idAddress,
+      }));
+    }
+  }, [address]);
   return (
     <>
       <div className="col-lg-12 row ">
         <div className="col-lg-9 col-9">
           <div className="billing-info mb-20">
             <label>{t("page_checkout.street_address")}</label>
-            <select name="idDireccion" onChange={handleChange} className="form-select">
-              <option >{t("page_checkout.select_address")}</option>
-              {address &&  address.map((direccion) => (
-                <option key={`${direccion.id}-${direccion.name}`} value={direccion.id}>
-                  {direccion.name}
-                </option>
-              ))}
-              
+            <select
+              name="idDireccion"
+              onChange={handleChange}
+              className="form-select"
+            >
+              <option>{t("page_checkout.select_address")}</option>
+              {address &&
+                address.map((direccion) => (
+                  <option
+                    key={`${direccion.idAddress}-${direccion.name}`}
+                    value={direccion.idAddress}
+                    selected={
+                      address[address.length - 1].idAddress ===
+                      direccion.idAddress
+                    }
+                  >
+                    {direccion.name}
+                  </option>
+                ))}
             </select>
-    
           </div>
         </div>
         <div className="col-lg-2 col-2">
@@ -42,27 +57,28 @@ export const FormDireccionEntrega = ({
             type="button"
             className="button-active-hs btn-black fw-bold mt-1 px-4 py-2"
             onClick={() => {
-              showAddressNew ? setShowAddressNew(false) : setShowAddressNew(true);
+              showAddressNew
+                ? setShowAddressNew(false)
+                : setShowAddressNew(true);
             }}
           >
             <MapPinPlus />
-            </button>
+          </button>
         </div>
       </div>
 
-      {
-        showAddressNew && (
-          <div className="col-lg-12 col-md-12 mt-2">
-           <FormNuevaDireccion 
-             country={country}
-             formValues={formValues}
-             handleChange={handleChange}
-             errorsValidate={errorsValidate}
-           /> 
-          </div>
-        )
-      }
-    
+      {showAddressNew && (
+        <div className="col-lg-12 col-md-12 mt-2">
+          <FormNuevaDireccion
+            country={country}
+            formValues={formValues}
+            handleChange={handleChange}
+            errorsValidate={errorsValidate}
+            setShowAddressNew={setShowAddressNew}
+          />
+        </div>
+      )}
+
       <div className="col-lg-12 col-md-12 mt-2">
         <div className="additional-info-wrap">
           <h4>{t("page_checkout.additional_information")}</h4>
