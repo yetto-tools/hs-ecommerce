064852a808d21f9cd6ef7ccf4d79a01a41f1adc2
Erick Orlando Rashón González <erashon@disresa.com>
2025-03-31 20:20:55 -0600

    Mostrar Producto y validar existencia

--- src/components/header/sub-components/MenuCart.js
+++ src/components/header/sub-components/MenuCart.js
@@ -1,10 +1,13 @@
-import { Fragment, useEffect } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getDiscountPrice } from "../../../helpers/product";
 import { deleteFromCart } from "../../../store/slices/cart-slice";
 import { useTranslation } from "react-i18next";
 import { ROOT_IMAGE } from "../../../config";
+import ProductModal2 from "../../product/ProductModal2";
+import { fetchArticleDetail } from "../../../hooks/use-FetchArticles";
+import { setLoading } from "../../../store/slices/articleDetail-slice";
 
 const MenuCart = () => {
   const dispatch = useDispatch();
@@ -12,118 +15,172 @@ const MenuCart = () => {
   const { cartItems } = useSelector((state) => state.cart);
   let cartTotalPrice = 0;
   const { t, i18n } = useTranslation();
+  const [modalShow, setModalShow] = useState(false);
+  const cartRef = useRef(null);
 
-  
+  const handleProductQuickView = async (e, product) => {
+    if (cartRef.current) {
+      cartRef.current.classList.remove("active"); // Remueve la clase 'active' del carrito
+    }
+    try {
+      dispatch(setLoading(true));
+      await dispatch(fetchArticleDetail(product.sku));
+    } catch (error) {
+      console.error("Error al cargar detalle:", error);
+    } finally {
+      setModalShow(true);
+      dispatch(setLoading(false));
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []); // Array de dependencias vacío para que solo se aplique al montar y desmontar
+  function handleClickOutside(event) {
+    if (cartRef.current && !cartRef.current.contains(event.target)) {
+      // revove class active
+      cartRef.current.classList.remove("active");
+    }
+  }
 
   return (
-    <div className="shopping-cart-content">
-      {cartItems && cartItems.length > 0 ? (
-        <Fragment>
-          <ul>
-            {cartItems.map((item) => {
-              const discountedPrice = getDiscountPrice(
-                item.price,
-                item.discount
-              );
-              const finalProductPrice = (
-                item.price * currency.currencyRate
-              ).toFixed(2);
-              const finalDiscountedPrice = (
-                discountedPrice * currency.currencyRate
-              ).toFixed(2);
+    <Fragment>
+      <div className="shopping-cart-content" ref={cartRef}>
+        {cartItems && cartItems.length > 0 ? (
+          <Fragment>
+            <ul>
+              {cartItems.map((item) => {
+                const discountedPrice = getDiscountPrice(
+                  item.price,
+                  item.discount
+                );
+                const finalProductPrice = (
+                  item.price * currency.currencyRate
+                ).toFixed(2);
+                const finalDiscountedPrice = (
+                  discountedPrice * currency.currencyRate
+                ).toFixed(2);
 
-              discountedPrice != null
-                ? (cartTotalPrice += finalDiscountedPrice * item.quantity)
-                : (cartTotalPrice += finalProductPrice * item.quantity);
+                discountedPrice != null
+                  ? (cartTotalPrice += finalDiscountedPrice * item.quantity)
+                  : (cartTotalPrice += finalProductPrice * item.quantity);
 
-              return (
-                <li className="single-shopping-cart" key={item.cartItemId}>
-                  <div className="shopping-cart-img">
-                    <Link to={process.env.PUBLIC_URL + "/product/" + item.id}>
-                      <img
-                        alt=""
-                        src={
-                          ROOT_IMAGE + item?.images[0] ||
-                          ROOT_IMAGE + item?.image
-                        }
-                        className="img-fluid"
-                      />
-                    </Link>
-                  </div>
-                  <div className="shopping-cart-title">
-                    <h4 className="mb-4">
-                      <Link to={process.env.PUBLIC_URL + "/product/" + item.id}>
-                        {" "}
-                        {item.name}{" "}
-                      </Link>
-                    </h4>
-                    <h6>
-                      {t("page_cart.th_qty")}: {item?.quantity}
-                    </h6>
-                    <h6>
-                      {"Precio"}:{" "}
-                      {discountedPrice !== null
-                        ? new Intl.NumberFormat(i18n.language, {
-                            style: "currency",
-                            currency: currency.currencyName,
-                          }).format(finalDiscountedPrice)
-                        : new Intl.NumberFormat(i18n.language, {
-                            style: "currency",
-                            currency: currency.currencyName,
-                          }).format(finalProductPrice)}
-                    </h6>
-                    <span className="">
-                      {item.selectedProductColor && item.selectedProductSize ? (
-                        <div className="cart-item-variation">
-                          <h6>Color: {item.selectedProductColor}</h6>
-                          <h6>
-                            {t("general_words.size")}:{" "}
-                            {item.selectedProductSize}
-                          </h6>
-                        </div>
-                      ) : (
-                        ""
-                      )}
-                    </span>
-                  </div>
-                  <div className="shopping-cart-delete">
-                    <button
-                      onClick={() => dispatch(deleteFromCart(item.cartItemId))}
-                    >
-                      <i className="fa fa-times-circle" />
-                    </button>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-          <div className="shopping-cart-total">
-            <h4>
-              Total :{" "}
-              <span className="shop-total">
-                {new Intl.NumberFormat(i18n.language, {
-                  style: "currency",
-                  currency: currency.currencyName,
-                }).format(cartTotalPrice)}
-              </span>
-            </h4>
-          </div>
-          <div className="shopping-cart-btn btn-hover text-center">
-            <Link className="default-btn" to={process.env.PUBLIC_URL + "/cart"}>
-              {t("general_words.view_cart")}
-            </Link>
-            <Link
-              className="default-btn"
-              to={process.env.PUBLIC_URL + "/checkout"}
-            >
-              {t("checkout")}
-            </Link>
-          </div>
-        </Fragment>
-      ) : (
-        <p className="text-center">{t("no_items_cart")}</p>
-      )}
-    </div>
+                return (
+                  <li className="single-shopping-cart" key={item.cartItemId}>
+                    <div className="shopping-cart-img">
+                      <button
+                        id={item.id}
+                        name={item.code}
+                        type="button"
+                        onClick={(e) => handleProductQuickView(e, item)}
+                      >
+                        <img
+                          alt={item.name}
+                          src={
+                            ROOT_IMAGE + item?.images[0] ||
+                            ROOT_IMAGE + item?.image
+                          }
+                          width={70}
+                          className="img-fluid"
+                        />
+                      </button>
+                    </div>
+                    <div className="shopping-cart-title">
+                      <h4 className="mb-4">
+                        <span className="text-left text-black fs-6">
+                          {" "}
+                          {item.name}{" "}
+                          <small className="text-xs text-muted">
+                            {item.code}
+                          </small>
+                        </span>
+                      </h4>
+                      <h6>
+                        {t("page_cart.th_qty")}: {item?.quantity}
+                      </h6>
+                      <h6>
+                        {"Precio"}:{" "}
+                        {discountedPrice !== null
+                          ? new Intl.NumberFormat(i18n.language, {
+                              style: "currency",
+                              currency: currency.currencyName,
+                            }).format(finalDiscountedPrice)
+                          : new Intl.NumberFormat(i18n.language, {
+                              style: "currency",
+                              currency: currency.currencyName,
+                            }).format(finalProductPrice)}
+                      </h6>
+                      <span className="">
+                        {item.selectedProductColor &&
+                        item.selectedProductSize ? (
+                          <div className="cart-item-variation">
+                            <h6>Color: {item.selectedProductColor}</h6>
+                            <h6>
+                              {t("general_words.size")}:{" "}
+                              {item.selectedProductSize}
+                            </h6>
+                          </div>
+                        ) : (
+                          ""
+                        )}
+                      </span>
+                    </div>
+
+                    <div className="shopping-cart-delete">
+                      <button
+                        onClick={(e) => {
+                          handleClickOutside(e);
+                          dispatch(deleteFromCart(item.cartItemId));
+                        }}
+                      >
+                        <i className="fa fa-times-circle" />
+                      </button>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="shopping-cart-total">
+              <h4>
+                Total :{" "}
+                <span className="shop-total">
+                  {new Intl.NumberFormat(i18n.language, {
+                    style: "currency",
+                    currency: currency.currencyName,
+                  }).format(cartTotalPrice)}
+                </span>
+              </h4>
+            </div>
+            <div className="shopping-cart-btn btn-hover text-center">
+              <Link
+                className="default-btn"
+                to={process.env.PUBLIC_URL + "/cart"}
+              >
+                {t("general_words.view_cart")}
+              </Link>
+              <Link
+                className="default-btn"
+                to={process.env.PUBLIC_URL + "/checkout"}
+              >
+                {t("checkout")}
+              </Link>
+            </div>
+          </Fragment>
+        ) : (
+          <p className="text-center">{t("no_items_cart")}</p>
+        )}
+      </div>
+      <ProductModal2
+        show={modalShow}
+        onHide={() => {
+          setModalShow(false);
+        }}
+        currency={currency}
+      />
+    </Fragment>
   );
 };
 
