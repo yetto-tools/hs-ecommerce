3a27e783ce94c3531343ee05c0a204b035c9284e
Erick Orlando Rashón González <erashon@disresa.com>
2025-04-25 15:23:04 -0600

    cambio de version

--- public/config-params.js
+++ public/config-params.js
@@ -3,6 +3,6 @@ export const APP_API_URL = "https://test-apihs.yettotools.com";
 export const APP_API_VERSION = "v1";
 export const APP_PORT = "3000";
 export const APP_API_NIT = "https://api.hypestreet.dssolutionsgt.com";
-export const APP_VERSION = "1.23.6";
+export const APP_VERSION = "1.23.7";
 export const APP_NAME = "HypeStreet";
 export const APP_ENV = "develpment" || "production";
