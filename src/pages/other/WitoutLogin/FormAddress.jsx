cb6bf40a955a7da34acfb54a90885c4a90503a3d
Erick Orlando Rashón González <erashon@disresa.com>
2025-05-11 20:14:25 -0600

    cambios para exter modelo y logica a hooks

--- src/pages/other/WitoutLogin/FormAddress.jsx
+++ src/pages/other/WitoutLogin/FormAddress.jsx
@@ -1,38 +1,22 @@
 import clsx from "clsx";
-import { useTranslation } from "react-i18next";
-import { useDeliveryAddressForm } from "./useDeliveryAddressForm";
 
-
-const FormDeliveryAddress = ({ isFirstRender }) => {
+const FormDeliveryAddress = ({ formData, isValid, handleChange }) => {
   const title = "Datos de Envío";
-  const { t } = useTranslation();
-
-  const {
-    formData,
-    isValid,
-    handleChange,
-  } = useDeliveryAddressForm({
-    idUsuario: 1,
-    nombre: "",
-    idPais: 1,
-    idDepartamento: 7,
-    idMunicipio: 45,
-    telefono: "",
-    direccion: "",
-    observaciones: "",
-  });
 
   return (
     <section id="delivery_address">
       {title && (
-        <h4 className="mb-3 font-semibold" onClick={() => console.log(formData)}>
+        <h4
+          className="mb-3 font-semibold"
+          onClick={() => console.log(formData)}
+        >
           {title}
         </h4>
       )}
       <hr />
 
-      <div className={clsx("mb-3", !isValid.nombre && !isFirstRender && "text-danger fw-bold")}>
-        <label className="form-label">Nombre *</label>
+      <div className={clsx("mb-3", !isValid.nombre && "text-danger fw-bold")}>
+        <label className="form-label mb-0">Nombre *</label>
         <div className="validation">
           <input
             type="text"
@@ -46,41 +30,55 @@ const FormDeliveryAddress = ({ isFirstRender }) => {
           />
           <span className="validity"></span>
         </div>
-        {!isValid.nombre && !isFirstRender && <small>Campo requerido</small>}
+        {!isValid.nombre && <small>Campo requerido</small>}
       </div>
 
       <div className="row">
-        <div className={clsx("mb-3 col-md-12", !isValid.telefono && !isFirstRender && "text-danger fw-bold")}>
-          <label className="form-label">Teléfono *</label>
-          <input
-            type="text"
-            name="telefono"
-            value={formData.telefono}
-            onChange={handleChange}
-            className="form-control"
-            placeholder="Teléfono"
-            required
-            autoComplete="off"
-          />
-          {!isValid.telefono && !isFirstRender && <small>Campo requerido</small>}
+        <div
+          className={clsx(
+            "mb-3 col-md-12",
+            !isValid.telefono && "text-danger fw-bold"
+          )}
+        >
+          <label className="form-label mb-0">Teléfono *</label>
+
+          <div className="validation">
+            <input
+              type="text"
+              name="telefono"
+              value={formData.telefono}
+              onChange={handleChange}
+              className="form-control"
+              placeholder="Teléfono"
+              required
+              autoComplete="off"
+            />
+            <span className="validity"></span>
+          </div>
+          {!isValid.telefono && <small>Campo requerido</small>}
         </div>
       </div>
 
-      <div className={clsx("mb-3", !isValid.direccion && !isFirstRender && "text-danger fw-bold")}>
-        <label className="form-label">Dirección *</label>
-        <textarea
-          name="direccion"
-          value={formData.direccion}
-          onChange={handleChange}
-          className="form-control"
-          placeholder="Dirección"
-          required
-        />
-        {!isValid.direccion && !isFirstRender && <small>Campo requerido</small>}
+      <div
+        className={clsx("mb-3", !isValid.direccion && "text-danger fw-bold")}
+      >
+        <label className="form-label mb-0">Dirección *</label>
+        <div className="validation">
+          <textarea
+            name="direccion"
+            value={formData.direccion}
+            onChange={handleChange}
+            className="form-control"
+            placeholder="Dirección"
+            required
+          />
+          <span className="validity"></span>
+        </div>
+        {!isValid.direccion && <small>Campo requerido</small>}
       </div>
 
       <div className="mb-3">
-        <label className="form-label">Observaciones</label>
+        <label className="form-label mb-0">Observaciones</label>
         <textarea
           name="observaciones"
           value={formData.observaciones}
