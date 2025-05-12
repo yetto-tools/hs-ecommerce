64adb1f074286024f6c4f09f6699ed4e9af9aea2
Erick Orlando Rashón González <erashon@disresa.com>
2025-05-11 20:13:21 -0600

    modelo y adapter para usuario anonimo

--- src/adapters/users.js
+++ src/adapters/users.js
@@ -25,3 +25,40 @@ export const adapterUserRegister = (user) => {
     origin: user.originCreation || "web",
   };
 };
+
+export const CustomerAnonymous = {
+  idUsuario: 1,
+  nombre: "",
+  idPais: 1,
+  idDepartamento: 7,
+  idMunicipio: 45,
+  telefono: "",
+  direccion: "",
+  observaciones: "",
+};
+
+export const adapterUserAnonymous = (user = null) => {
+  if (!user)
+    return {
+      idUsuario: 1,
+      nombre: "",
+      idPais: 1,
+      idDepartamento: 7,
+      idMunicipio: 45,
+      telefono: "",
+      direccion: "",
+      observaciones: "",
+    };
+  else {
+    return {
+      idUsuario: user.id,
+      nombre: user.name,
+      idPais: user.idCountry,
+      idDepartamento: user.idState,
+      idMunicipio: user.idCity,
+      telefono: user.phone,
+      direccion: user.address,
+      observaciones: user.comment,
+    };
+  }
+};
