ac203495a90c74ceb11b5329dd9df77212710a47
Erick Orlando Rashón González <erashon@disresa.com>
2025-05-11 20:13:36 -0600

    extraccion de logica a custome hook

--- src/pages/other/WitoutLogin/CheckoutWithoutLogin.jsx
+++ src/pages/other/WitoutLogin/CheckoutWithoutLogin.jsx
@@ -1,5 +1,5 @@
 // BacCheckout.jsx
-import { Fragment, useEffect, useMemo, useState } from "react";
+import { Fragment, use, useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -12,6 +12,8 @@ import { Breadcrumb } from "react-bootstrap";
 import LayoutOne from "../../../layouts/LayoutOne";
 import { getDiscountPrice } from "../../../helpers/product";
 import FormDeliveryAddress from "./FormAddress";
+import { useDeliveryAddressForm } from "./useDeliveryAddressForm";
+import { useCheckoutWithoutLogin } from "./useCheckoutWithoutLogin";
 
 const CheckoutWithoutLogin = () => {
   const { t, i18n } = useTranslation();
@@ -22,11 +24,19 @@ const CheckoutWithoutLogin = () => {
   const { usuario, address } = useSelector((state) => state.usuario);
   const [formIsValid, setFormIsValid] = useState(false);
 
-  const handleSubmitInvoces = ()=>{
-    console.log('submit');
-  }
-  let cartTotalPrice = 0;
-  
+  const { formData, isValid, isFormValid, handleChange } =
+    useDeliveryAddressForm();
+
+  const { cartTotalPrice, handleSendOrder, loadingOrder } =
+    useCheckoutWithoutLogin();
+
+  const handleSubmitInvoces = (e) => {
+    handleSendOrder(e);
+  };
+  const handleDEBUG = () => {
+    console.log(isValid);
+  };
+
   return (
     <Fragment>
       <SEO
@@ -45,57 +55,69 @@ const CheckoutWithoutLogin = () => {
             {cartItems && cartItems.length >= 1 ? (
               <div className="row">
                 <div className="col-lg-6">
-                  <div className="billing-info-wrap mb-30">
+                  <div className="billing-info-wrap mb-5">
                     <div className="row">
-                      <div className="border-bottom p-4 rounded mt-5">
-                       <FormDeliveryAddress />
+                      <div className="col-lg-10">
+                        <div className="border-bottom p-4 rounded mt-0">
+                          <FormDeliveryAddress
+                            formData={formData}
+                            isValid={isValid}
+                            handleChange={handleChange}
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
 
-            
                   <br />
-                  <div className="row">
-                    <div className="mt-5">
-                      <h3
->
-                        Confirmar Orden de Compra
-                      </h3>
-                      <hr />
-                      <div
-                        className="row"
-                        style={{
-                          opacity: !formIsValid ? 0.3 : 1,
-                          pointerEvents: !formIsValid ? "none" : "auto",
-                        }}
-                      >
-                        <div className="col-lg-12">
-                          <button
-                            type="submit"
-                            className="button-active-hs btn-black w-100 d-flex justify-content-center align-items-center gap-2 py-2"
-                            disabled={!formIsValid || disabledSendButton}
-                            onClick={handleSubmitInvoces}
+                  <div className="billing-info-wrap mb-5">
+                    <div className="row">
+                      <div className="col-lg-10">
+                        <div className="border-bottom p-4 rounded mt-0">
+                          <h3 onClick={() => handleDEBUG()}>
+                            Confirmar Orden de Compra
+                          </h3>
+                          <hr />
+                          <div
+                            className="row"
+                            style={{
+                              opacity: !isFormValid ? 0.3 : 1,
+                              pointerEvents: !isFormValid ? "none" : "auto",
+                            }}
                           >
-                            <span>
-                              {formIsValid
-                                ? t("send_message")
-                                : t("page_checkout.complete_fields")}
-                            </span>
+                            <div className="col-lg-10 mx-auto">
+                              <div className="row col-lg-12 mx-auto">
+                                <button
+                                  type="submit"
+                                  className="button-active-hs btn-black w-100 d-flex justify-content-center align-items-center gap-2 py-2"
+                                  disabled={!isFormValid || loadingOrder}
+                                  onClick={handleSubmitInvoces}
+                                >
+                                  <span>
+                                    {isFormValid
+                                      ? t("send_message")
+                                      : t("page_checkout.complete_fields")}
+                                  </span>
 
-                            {disabledSendButton ? (
-                              <Loader2 className="animate-spin" />
-                            ) : (
-                              <Send className="position-relative" />
+                                  {loadingOrder ? (
+                                    <Loader2 className="animate-spin" />
+                                  ) : (
+                                    <Send className="position-relative" />
+                                  )}
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="row col-lg-12 mx-auto">
+                            {!isFormValid && (
+                              <p className="text-danger text-center mt-2">
+                                {t("page_checkout.complete_all_fields")}
+                              </p>
                             )}
-                          </button>
+                          </div>
                         </div>
                       </div>
-                    </div>{" "}
-                    {!formIsValid && usuario && (
-                      <p className="text-danger mt-2">
-                        {t("page_checkout.complete_all_fields")}
-                      </p>
-                    )}
+                    </div>
                   </div>
                 </div>
 
