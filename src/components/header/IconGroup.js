c89997a1c52c299cc294e73f1d0ed880971fe042
Erick Orlando Rashón González <erashon@disresa.com>
2025-08-11 19:41:09 -0600

    add icon help

--- src/components/header/IconGroup.js
+++ src/components/header/IconGroup.js
@@ -155,6 +155,71 @@ const IconGroup = ({ iconWhiteClass }) => {
         </div>
       </div>
 
+      <div className="same-style header-compare d-none d-lg-block ms-2">
+        <div className="account-setting d-none d-lg-block">
+          <button
+            type="button"
+            id="account-button"
+            name="account"
+            className="icon-cart"
+            onClick={(e) => handleClick(e)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="32px"
+              viewBox="0 -960 960 960"
+              width="32px"
+              fill="currentColor"
+            >
+              <path d="M478-240q21 0 35.5-14.5T528-290q0-21-14.5-35.5T478-340q-21 0-35.5 14.5T428-290q0 21 14.5 35.5T478-240Zm-36-154h74q0-33 7.5-52t42.5-52q26-26 41-49.5t15-56.5q0-56-41-86t-97-30q-57 0-92.5 30T342-618l66 26q5-18 22.5-39t53.5-21q32 0 48 17.5t16 38.5q0 20-12 37.5T506-526q-44 39-54 59t-10 73Zm38 314q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+            </svg>
+          </button>
+          <div
+            className="account-dropdown"
+            style={{ width: "145px" }}
+            ref={userRef}
+          >
+            <ul>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/login"}>
+                  <span className="text-ellipsis">
+                    {(usuario && usuario.id && `${usuario.email}`) ||
+                      t("icon_group.login")}
+                  </span>
+                </Link>
+              </li>
+              {!isLoggedIn && (
+                <li>
+                  <Link to={process.env.PUBLIC_URL + "/registrarse"}>
+                    <span className="text-ellipsis">
+                      {t("icon_group.register")}
+                    </span>
+                  </Link>
+                </li>
+              )}
+
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/mi-cuenta"}>
+                  {t("icon_group.my_account")}
+                </Link>
+              </li>
+              {isLoggedIn && (
+                <li>
+                  <Link
+                    to={""}
+                    onClick={handleLogout}
+                    className="text-sm text-black"
+                  >
+                    {t("icon_group.logout")}
+                    <i className="pe-7s-exit" />
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+
       <div className="same-style cart-wrap d-none d-lg-block ms-2">
         <button
           type="button"
