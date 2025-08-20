b216bb3d82d054283d4aa92ff400f2c053fef931
Erick Rashon <ergonzalez209@gmail.com>
2025-08-19 20:43:08 -0600

    fix resumen compras

--- src/pages/other/ResumenCompra.jsx
+++ src/pages/other/ResumenCompra.jsx
@@ -1,29 +1,39 @@
+import clsx from "clsx";
+
 export const ResumenCompra = ({
   cartItems,
-  cartTotalPrice,
   currency,
   getDiscountPrice,
   t,
   i18n,
 }) => {
+  // Calcular total
+  const total = cartItems.reduce((acc, cartItem) => {
+    const discountedPrice = getDiscountPrice(cartItem.price, cartItem.discount);
+    const finalProductPrice = cartItem.price * currency.currencyRate;
+    const finalDiscountedPrice = discountedPrice * currency.currencyRate;
 
-  const Total = cartTotalPrice.toFixed(2);
+    return (
+      acc +
+      (discountedPrice != null
+        ? finalDiscountedPrice * cartItem.quantity
+        : finalProductPrice * cartItem.quantity)
+    );
+  }, 0);
 
   return (
     <div className="your-order-area sticky-column">
       <h3>{t("page_checkout.your_order")}</h3>
       <div className="your-order-wrap gray-bg-4">
         <div className="your-order-product-info">
-          <div
-            className="your-order-top"
-            onClick={() => console.log(cartItems)}
-          >
+          <div className="your-order-top">
             <ul>
               <li className="fw-bold">{t("page_checkout.product")}</li>
               <li className="fw-bold">{t("page_checkout.quantity")}</li>
               <li className="fw-bold">Total</li>
             </ul>
           </div>
+
           <div className="your-order-middle">
             <ul>
               {cartItems.map((cartItem, key) => {
@@ -38,21 +48,43 @@ export const ResumenCompra = ({
                   discountedPrice * currency.currencyRate
                 ).toFixed(2);
 
-                discountedPrice != null
-                  ? (cartTotalPrice += finalDiscountedPrice * cartItem.quantity)
-                  : (cartTotalPrice += finalProductPrice * cartItem.quantity);
                 return (
                   <li key={key}>
                     <span className="order-middle-left text-truncate">
                       {cartItem.name} - {cartItem.size}
-                      <p className="text-xs fw-bold">
-                        {new Intl.NumberFormat(i18n.language, {
-                          style: "currency",
-                          currency: currency.currencyName,
-                        }).format(finalProductPrice)}
-                      </p>
+                      <div className="d-flex justify-content-start gap-2">
+                        {discountedPrice !== null && discountedPrice > 0 ? (
+                          <>
+                            {/* Precio con descuento */}
+                            <p className="text-xs fw-bold">
+                              {new Intl.NumberFormat(i18n.language, {
+                                style: "currency",
+                                currency: currency.currencyName,
+                              }).format(finalDiscountedPrice)}
+                            </p>
+
+                            {/* Precio original tachado */}
+                            <p className="text-xs fw-bold muted tached">
+                              {new Intl.NumberFormat(i18n.language, {
+                                style: "currency",
+                                currency: currency.currencyName,
+                              }).format(finalProductPrice)}
+                            </p>
+                          </>
+                        ) : (
+                          // Solo precio normal si no hay descuento
+                          <p className="text-xs fw-bold">
+                            {new Intl.NumberFormat(i18n.language, {
+                              style: "currency",
+                              currency: currency.currencyName,
+                            }).format(finalProductPrice)}
+                          </p>
+                        )}
+                      </div>
+
                     </span>
-                    <strong> {cartItem.quantity} </strong>
+
+                    <strong>{cartItem.quantity}</strong>
 
                     <span className="order-price fw-bold">
                       {discountedPrice !== null
@@ -70,6 +102,7 @@ export const ResumenCompra = ({
               })}
             </ul>
           </div>
+
           <div className="your-order-bottom">
             <ul>
               <li className="your-order-shipping">
@@ -78,6 +111,7 @@ export const ResumenCompra = ({
               <li>{t("page_checkout.free_shipping")}</li>
             </ul>
           </div>
+
           <div className="your-order-total">
             <ul>
               <li className="order-total fw-bold">Total</li>
@@ -85,12 +119,11 @@ export const ResumenCompra = ({
                 {new Intl.NumberFormat(i18n.language, {
                   style: "currency",
                   currency: currency.currencyName,
-                }).format(Total)}
+                }).format(total.toFixed(2))}
               </li>
             </ul>
           </div>
         </div>
-        <div className="payment-method"></div>
       </div>
     </div>
   );
